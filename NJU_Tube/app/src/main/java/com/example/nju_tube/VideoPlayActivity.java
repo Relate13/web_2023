93333c61cd8cc3aca951c8829855d90b08a1ad06
Qingyun Yang <qingyun_yang@foxmail.com>
2023-05-20 22:27:00 +0800

    FEAT: play video with exoplayer

--- NJU_Tube/app/src/main/java/com/example/nju_tube/VideoPlayActivity.java
+++ NJU_Tube/app/src/main/java/com/example/nju_tube/VideoPlayActivity.java
@@ -1,18 +1,17 @@
 package com.example.nju_tube;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.MediaController;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.ui.StyledPlayerView;
 
 /** 视频播放页面 */
 public class VideoPlayActivity extends AppCompatActivity {
@@ -22,7 +21,10 @@ public class VideoPlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_play);
 
-        VideoView videoView = findViewById(R.id.video_view);
+        StyledPlayerView videoView = findViewById(R.id.video_player);
+        Player videoPlayer = new ExoPlayer.Builder(this).build();
+        videoView.setPlayer(videoPlayer);
+        videoView.setBackgroundColor(Color.BLACK);
 
         Intent intent = getIntent();
         TextView videoTitle = findViewById(R.id.video_info_title);
@@ -33,20 +35,8 @@ public class VideoPlayActivity extends AppCompatActivity {
         videoAuthor.setText(intent.getStringExtra("Author"));
 
         Uri uri = Uri.parse(intent.getStringExtra("VideoURL"));
-        videoView.setVideoURI(uri);
-
-        // 视频未播放时展示视频封面
-        ImageView coverView = findViewById(R.id.cover_image_view);
-        Glide.with(this).load(intent.getStringExtra("CoverURL")).into(coverView);
-        videoView.setOnPreparedListener(mp -> mp.setOnInfoListener((mp1, what, extra) -> {
-            //播放第一帧时设置图片消失
-            if (what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START)
-                coverView.setVisibility(View.GONE);
-            return true;
-        }));
-
-        MediaController controller = new MediaController(this);
-        videoView.setMediaController(controller);
-        controller.setAnchorView(videoView);
+        videoPlayer.addMediaItem(MediaItem.fromUri(uri));
+        videoPlayer.prepare();
     }
+
 }

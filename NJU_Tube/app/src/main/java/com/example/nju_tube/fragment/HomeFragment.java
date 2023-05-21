98c93b34f037abd5cd561d7bf8c184a7cd164cad
Qingyun Yang <qingyun_yang@foxmail.com>
2023-05-21 16:29:52 +0800

    FEAT: my uploads and my favorites

--- NJU_Tube/app/src/main/java/com/example/nju_tube/fragment/HomeFragment.java
+++ NJU_Tube/app/src/main/java/com/example/nju_tube/fragment/HomeFragment.java
@@ -1,32 +1,31 @@
 package com.example.nju_tube.fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-
+import com.example.nju_tube.NJUTube;
 import com.example.nju_tube.R;
+
+import java.util.Objects;
 
 
 public class HomeFragment extends Fragment {
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         // Inflate the layout for this fragment
-        FrameLayout video_frame_layout = view.findViewById(R.id.home_video_view_frame);
+        TextView userName = view.findViewById(R.id.home_user_name);
+        userName.setText(((NJUTube) Objects.requireNonNull(getActivity()).getApplication()).getUserName());
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-        transaction.replace(R.id.home_video_view_frame, new UploadedByMeFragment("me"));
+        transaction.replace(R.id.home_video_view_frame, new UploadedByMeFragment());
         transaction.commit();
         return view;
     }
--- NJU_Tube/app/src/main/java/com/example/nju_tube/fragment/UploadedByMeFragment.java
+++ NJU_Tube/app/src/main/java/com/example/nju_tube/fragment/UploadedByMeFragment.java
@@ -1,25 +1,67 @@
 package com.example.nju_tube.fragment;
 
 import android.os.Handler;
+import android.widget.Toast;
 
+import com.example.nju_tube.HttpUtils;
+import com.example.nju_tube.NJUTube;
+import com.example.nju_tube.R;
 import com.example.nju_tube.ui.UserItem;
 import com.example.nju_tube.ui.VideoItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class UploadedByMeFragment extends ExploreFragment{
-    private final String userName;
-    public UploadedByMeFragment(String whoAmI){
-        userName=whoAmI;
-    }
     @Override
     public void generateVideoList(Handler handler) {
         List<VideoItem> newVideos = new ArrayList<>();
-        newVideos.add(new VideoItem(1, "1", "1", "1", new UserItem(1,userName), "1", 1, 1, true));
-        newVideos.add(new VideoItem(1, "1", "1", "1", new UserItem(1,userName), "1", 1, 1, true));
-        newVideos.add(new VideoItem(1, "1", "1", "1", new UserItem(1,userName), "1", 1, 1, true));
-        newVideos.add(new VideoItem(1, "1", "1", "1", new UserItem(1,userName), "1", 1, 1, true));
-        videoItemAdapter.addData(newVideos, handler);
+        String serverUrl = getString(R.string.server_url);
+        String uploadListUrl = serverUrl+getString(R.string.my_uploads);
+        uploadListUrl += "?token="+((NJUTube)(Objects.requireNonNull(getActivity()).getApplication())).getToken();
+        uploadListUrl += "&user_id="+((NJUTube)(Objects.requireNonNull(getActivity()).getApplication())).getUserId();
+        try {
+            // 通过网络请求获得视频列表json数据
+            HttpURLConnection connection = (HttpURLConnection) new URL(uploadListUrl).openConnection();
+            connection.setConnectTimeout(5000);
+            connection.setRequestMethod("GET");
+            InputStream is = connection.getInputStream();
+            JSONObject jsonObject = HttpUtils.readInputStream(is);
+            connection.disconnect();
+
+            int statusCode = jsonObject.getJSONObject("response").getInt("status_code");
+            if (statusCode != 0) {
+                handler.post(() -> Toast.makeText(getContext(), getString(R.string.something_wrong), Toast.LENGTH_SHORT).show());
+                return;
+            }
+            // 解析视频列表
+            JSONArray videoList = jsonObject.getJSONArray("video_list");
+            for (int i=0; i<videoList.length(); ++i) {
+                // 处理视频信息并使其转换为VideoItem对象
+                JSONObject rawVideoItem = videoList.getJSONObject(i);
+                JSONObject rawUserItem = rawVideoItem.getJSONObject("author");
+                UserItem userItem = new UserItem((int) rawUserItem.get("id"), (String) rawUserItem.get("name"));
+                String uploadDate = (String) rawVideoItem.get("upload_date");
+                String[] splitDate = uploadDate.split("-"); // 服务端返回的日期类似于2023-5-17-22-00
+                uploadDate = splitDate[0]+"年"+splitDate[1]+"月"+splitDate[2]+"日"+" "+splitDate[3]+":"+splitDate[4]; // 重新格式化日期字符串
+
+                VideoItem videoItem = new VideoItem((int) rawVideoItem.get("id"), serverUrl+rawVideoItem.get("play_url"),
+                        serverUrl+rawVideoItem.get("cover_url"), (String) rawVideoItem.get("title"), userItem,
+                        uploadDate, (int) rawVideoItem.get("favorite_count"),
+                        (int) rawVideoItem.get("comment_count"), (Boolean) rawVideoItem.get("is_favorite"));
+                newVideos.add(videoItem);
+            }
+            videoItemAdapter.addData(newVideos, handler); // 更新数据到View
+        }
+        catch (IOException | JSONException ignored) {}
     }
 }
